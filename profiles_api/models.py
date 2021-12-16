2082e3c7b66bf855d950525cdc0f66a08c534c7e
nerdgeeek <tsunil359@gmail.com>
2021-12-16 10:26:52 +0545

    created superuser manually

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -10,32 +10,25 @@ class UserProfileManager(BaseUserManager):
     def create_user(self, email, name, password = None):
         """Create a new user profile"""
 
-
         if not email:
             raise ValueError("User must have an email address")
-            #normalising email
-
-            email = self.normalise_email(email)
-            user = self.model(email = email , name = name)
-
-            user.set_password(password)
-            user.save(using = self.db)
-
-            return user
-
-    def create_superuser(self,email,name, password):
-            """Create and save a new superuser with given details"""
+        email = self.normalize_email(email)
+        user = self.model(email = email , name = name)
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
 
 
-            user =self.create_user(email,name, password)
-            user.is_superuser = True
-            user.is_staff = True
-            user.save()
-
-            return user
-
-
-
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user =self.create_user(email ,
+         name ,
+         password
+         )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using = self._db)
+        return user
 
 
 
