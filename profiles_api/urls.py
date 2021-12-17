deba51b4ef492e300239bb6188bfeca94f6e54a5
nerdgeeek <tsunil359@gmail.com>
2021-12-17 14:56:08 +0545

    error fixed

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,15 +2,11 @@ from django.urls import path, include
 from profiles_api import views
 from rest_framework.routers import DefaultRouter
 
-
-
+router= DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet ,'hello-viewset')
 
 urlpatterns=[
-  path('hello_view/', views.HelloApiView.as_view(), name='hello
-  path('',include(router.urls))
+  path('hello_view/', views.HelloApiView.as_view(), name='hello'),
+  path('',include(router.urls)),
 
 ]
-
-
-router= DefaultRouter()
-router.register('hello-viewset', views.HelloViewSet , base_name = 'hello-viewset')
