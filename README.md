dc8b7d4ec5e6adf7a16e77ac3b23f14de8773f9b
nerdgeeek <tsunil359@gmail.com>
2021-12-17 08:47:02 +0545

    added hello api view

--- README.md
+++ README.md
@@ -31,3 +31,23 @@ source ~/env/bin/activate
 
 found the error in the last project i.e devsearch learn that tags had an extra s like tagss
 its because the django automatically defines the classes as plural and if you name the model as tags then when django creates databases it pluralizes it like tagss
+
+
+APIView:  basic
+allows to define function that matches standard http methods:
+get
+post
+ put
+  patch
+   delete
+VIEWSet:
+provide control over logic
+calling other apis and working with local files
+
+
+wen to use:
+personal preference
+validating and returning result in same call
+need full control over logic
+processing files and rendering sync response
+call other apis with same request
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# we used views.py to link the templates
+"""for api view:::"""
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format = None):
+        """Returns a list of APIView features"""
+        an_apiview=[
+        'Uses HTTP methods as functions( get post put delete patch)',
+        'Is similar to traditional django views',
+        'Gives you most control over our app',
+        'Is mapped manually to Urls',
+
+
+        ]
+
+        return Response({'message':'hello',  'an_apiview': an_apiview})
