f143d156306f3c622c5e1c2dc38be5a06f7c5d7b
nerdgeeek <tsunil359@gmail.com>
2021-12-20 14:29:24 +0545

    app1

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/sunilthapa43/restApi'
 
-PROJECT_BASE_PATH='/usr/local/apps/restApi'
+PROJECT_BASE_PATH='/usr/local/apps/app1/restApi'
 
 echo "Installing dependencies..."
 apt-get update
